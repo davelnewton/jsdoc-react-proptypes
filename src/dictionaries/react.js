24e85d38abf0fbb41c4c6698cc0a700970a9902a
Dave Newton <davelnewton@gmail.com>
2018-06-27 13:57:32 -0400

    1/add-function-type-label Was missing function propType label

--- src/dictionaries/react.js
+++ src/dictionaries/react.js
@@ -1,71 +1,68 @@
-// React PropTypes
-
 const PropTypes = {
-    any: (prop) => {
-        prop.type.names.push('*');
-    },
-    array: (prop) => {
-        prop.type.names.push('Array');
-    },
-    bool: (prop) => {
-        prop.type.names.push('boolean');
-    },
-    func: (prop) => {
-    },
-    number: (prop) => {
-        prop.type.names.push('number');
-    },
-    object: (prop) => {
-        prop.type.names.push('Object');
-    },
-    string: (prop) => {
-        prop.type.names.push('string');
-    },
-    symbol: (prop) => {
-        prop.type.names.push('Symbol');
-    },
-    node: (prop) => {
-        prop.type.names.push('ReactNode');
-    },
-    element: (prop) => {
-        prop.type.names.push('ReactElement');
-    },
-    instanceOf: (prop, instanceName) => {
-        prop.type.names.push(instanceName);
-    },
-    oneOf: (prop, options) => {
-        options.forEach(option => prop.type.names.push(`'${option}'`));
-    },
-    oneOfType: (prop, types) => {
-        types.forEach(type => prop.type.names.push(type));
-    },
-    arrayOf: (prop, type) => {
-        if(typeof type == "undefined") {
-            prop.type.names.push(`Array`);
-        } else {
-            prop.type.names.push(`Array.<${type}>`);
-        }
-    },
-    objectOf: (prop, type) => {
-        if(typeof type == "undefined") {
-            prop.type.names.push(`Object`);
-        } else {
-            prop.type.names.push(`Object.<string, ${type}>`);
-        }
-    },
-    shape: (prop) => {
-        prop.type.names.push("Object");
-    },
-    isRequired: (prop) => {
-        delete prop.optional;
+  any: (prop) => {
+    prop.type.names.push('*');
+  },
+  array: (prop) => {
+    prop.type.names.push('Array');
+  },
+  bool: (prop) => {
+    prop.type.names.push('boolean');
+  },
+  func: (prop) => {
+    prop.type.names.push('function');
+  },
+  number: (prop) => {
+    prop.type.names.push('number');
+  },
+  object: (prop) => {
+    prop.type.names.push('Object');
+  },
+  string: (prop) => {
+    prop.type.names.push('string');
+  },
+  symbol: (prop) => {
+    prop.type.names.push('Symbol');
+  },
+  node: (prop) => {
+    prop.type.names.push('ReactNode');
+  },
+  element: (prop) => {
+    prop.type.names.push('ReactElement');
+  },
+  instanceOf: (prop, instanceName) => {
+    prop.type.names.push(instanceName);
+  },
+  oneOf: (prop, options) => {
+    options.forEach(option => prop.type.names.push(`'${option}'`));
+  },
+  oneOfType: (prop, types) => {
+    types.forEach(type => prop.type.names.push(type));
+  },
+  arrayOf: (prop, type) => {
+    if (typeof type == 'undefined') {
+        prop.type.names.push(`Array`);
+    } else {
+        prop.type.names.push(`Array.<${type}>`);
     }
+  },
+  objectOf: (prop, type) => {
+    if (typeof type == 'undefined') {
+        prop.type.names.push('Object');
+    } else {
+        prop.type.names.push('Object.<string, ${type}>');
+    }
+  },
+  shape: (prop) => {
+    prop.type.names.push('Object');
+  },
+  isRequired: (prop) => {
+    delete prop.optional;
+  }
 };
 
-const ReactDictionary = [
-    {
-        name: "PropTypes",
-        dict: PropTypes
-    }
-];
+const ReactDictionary = [{
+  name: 'PropTypes',
+  dict: PropTypes
+}];
 
 export default ReactDictionary;
